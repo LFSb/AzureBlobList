ec7be2b740005ba14cb4839a495e07c1a2b620bc
Lee Beenen <l.beenen@diract.nl>
2015-10-23 09:41:09 +0200

    Initial commit

---
+++ AzureBlobList/BlobDownloader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureBlobList
+{
+  public class BlobDownloader
+  {
+    private readonly CloudBlobContainer _container;
+
+    public BlobDownloader(CloudBlobContainer container)
+    {
+      _container = container;
+    }
+
+    public void SavePageBlob(CloudBlobContainer container, CloudBlob blob)
+    {
+      var blobRef = _container.GetPageBlobReference(blob.Name);
+
+      var path = Path.Combine(ConfigurationManager.AppSettings["BlobSavePath"], blob.Name);
+
+      Console.WriteLine("Will save to: {0}", path);
+
+      using (var fileStream = File.OpenWrite(path))
+      {
+        var sw = new Stopwatch();
+        sw.Start();
+
+        var bla = blobRef.DownloadToStreamAsync(fileStream);
+
+        while (bla.IsCompleted == false)
+        {
+          Console.WriteLine("Status:{0}-{1}", bla.Status, DateTime.Now.ToShortTimeString());
+
+          Thread.Sleep(1000);
+        }
+      }
+    }
+
+    public void SaveBlockBlob(CloudBlobContainer container, CloudBlockBlob pageBlob)
+    {
+      var blobRef = _container.GetBlockBlobReference(pageBlob.Name);
+
+      var path = Path.Combine(ConfigurationManager.AppSettings["BlobSavePath"], pageBlob.Name);
+
+      Console.WriteLine("Will save to: {0}", path);
+
+      using (var fileStream = File.OpenWrite(path))
+      {
+        var sw = new Stopwatch();
+        sw.Start();
+
+        var bla = blobRef.DownloadToStreamAsync(fileStream);
+
+        while (bla.IsCompleted == false)
+        {
+          Console.WriteLine("Status:{0}-{1}", bla.Status, DateTime.Now.ToShortTimeString());
+
+          Thread.Sleep(1000);
+        }
+      }
+    }
+  }
+}
---
+++ AzureBlobList/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace AzureBlobList
+{
+  class Program
+  {
+    private static BlobDownloader blobDownloader;
+
+    static void Main(string[] args)
+    {
+      // Retrieve storage account from connection string.
+      var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+
+      // Create the blob client.
+      var blobClient = storageAccount.CreateCloudBlobClient();
+
+      // Retrieve reference to a previously created container.
+      var container = blobClient.GetContainerReference("vhds");
+
+      blobDownloader = new BlobDownloader(container);
+
+      // Loop over items within the container and output the length and URI.
+      foreach (var item in container.ListBlobs(null, false))
+      {
+        if (item is CloudBlockBlob)
+        {
+          var blob = (CloudBlockBlob)item;
+
+          Console.WriteLine("Block blob of length {0}: {1}", blob.Properties.Length, blob.Uri);
+
+          HandleUserInput(container, blob);
+        }
+        else if (item is CloudPageBlob)
+        {
+          var pageBlob = (CloudPageBlob)item;
+
+          Console.WriteLine("Page blob of length {0}: {1}", pageBlob.Properties.Length, pageBlob.Uri);
+
+          HandleUserInput(container, pageBlob);
+        }
+        else if (item is CloudBlobDirectory)
+        {
+          var directory = (CloudBlobDirectory)item;
+
+          Console.WriteLine("Directory: {0}", directory.Uri);
+        }
+      }
+
+      Console.WriteLine("All done!");
+      Console.ReadLine();
+    }
+
+    private static void HandleUserInput(CloudBlobContainer container, CloudBlockBlob pageBlob)
+    {
+      Console.WriteLine("Save file? Y/N");
+
+      var result = Console.ReadLine();
+
+      if (String.IsNullOrEmpty(result))
+      {
+        return;
+      }
+
+      switch (result.ToLower())
+      {
+        case "y":
+          {
+            blobDownloader.SaveBlockBlob(container, pageBlob);
+          } break;
+        default:
+          {
+            Console.WriteLine("welp");
+            break;
+          }
+      }
+    }
+
+    private static void HandleUserInput(CloudBlobContainer container, CloudPageBlob pageBlob)
+    {
+      Console.WriteLine("Save file? Y/N");
+
+      var result = Console.ReadLine();
+
+      if (String.IsNullOrEmpty(result))
+      {
+        return;
+      }
+
+      switch (result.ToLower())
+      {
+        case "y":
+          {
+            blobDownloader.SavePageBlob(container, pageBlob);
+          } break;
+        default:
+          {
+            Console.WriteLine("welp");
+            break;
+          }
+      }
+    }
+  }
+}
